8a2a8ffd8584536aeb3c2280e7da0eae8bf6e163
Matt Bidewell <mattbidewell@Matts-MacBook-Pro.local>
2022-05-10 11:55:52 +0100

    handle heroku port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import * as crypto from "crypto";
 import { getMediumPosts } from "medium-articles";
 
 const username = "mattbidewell";
-const port = 3000;
+const port = process.env.PORT || 3000; // heroku provides its own port.
 const tempUrl = "https://ignoreme.com"
 
 async function getPosts(username) {
